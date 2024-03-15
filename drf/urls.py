46cd10c012cdafb4ce9f830d7481460968068530
Mats <matgus1983@gmail.com>
2024-03-15 14:38:39 +0000

    url clean up.

--- drf/urls.py
+++ drf/urls.py
@@ -7,22 +7,14 @@ from .views import CurrentProfileView
 
 urlpatterns = [
     path('', root_route),
-      
-
-
     path('admin/', admin.site.urls),
-
     path('users/', include('users.urls')), 
-
     path('api/', include('profiles.urls')),
     path('current-profile/', CurrentProfileView.as_view()),      
-
     path('', include('posts.urls')),
-
     path('', include('comments.urls')),
-
     path('', include('likes.urls')),
-    
     path('', include('followers.urls')),
+
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
